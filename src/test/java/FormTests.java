f99d7dfd536250762bee282dec4300050cd36c91
Alexkarav <djmyasoru@gmail.com>
2022-02-12 21:56:26 +0300

    Added pageObject and test

--- src/test/java/FormTests.java
+++ src/test/java/FormTests.java
@@ -1,8 +1,9 @@
+import pages.StudentRegistrationPageObject;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
+import java.util.Arrays;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -11,97 +12,65 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class FormTests {
+
     //You can put your variables
     private String firstName = "Alex";
     private String lastName = "Box";
-    private Gender gender = Gender.OTHER;
     private String email = "AlexBox@gmail.com";
     private String phone = "7123456890";
     private String year = "1990";
-    private String day = "001";
+    private String month = "January";
+    private String day = "1";
+    private String gender = "Other";
     private String[] subjects = new String[]{"Maths", "Accounting"};
-    private String[] hobbies = new String[]{"Sports", "Reading"};
+    private String[] hobbys = new String[]{"Sport", "Reading"};
     private String address = "Puskina AV. Colotuskina residence";
     private String photo = "Mem.png";
     private String state = "Rajasthan";
     private String city = "Jaipur";
 
-    @BeforeAll
 
+    @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
     }
 
-    private enum Gender {
-        MALE("Male"),
-        FEMALE("Female"),
-        OTHER("Other");
-
-        private String selectorValue;
-
-        Gender(String selector) {
-            this.selectorValue = selector;
-        }
-    }
-
     @Test
-    void testForm() {
-        open("/automation-practice-form");
-        $(".main-header").shouldHave(text("Practice form"));
-
-        //Name
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(email);
-        $(byText(gender.selectorValue)).click();
-        $("#userNumber").setValue(phone);
-        $("#dateOfBirthInput").click();
-
-        //Date
-        $(".react-datepicker__year-select").selectOptionByValue(year);
-        $(".react-datepicker__month-select").selectOptionByValue("0"); // 0-11 month 0-January etc
-        $(".react-datepicker__day.react-datepicker__day--" + day).click();
-
-        //Subjects
-        for (String subject : subjects){
-            $("#subjectsInput").setValue(subject.substring(0,2)).click();
-            $(byText(subject)).click();
-        }
-        //Hobby
-        for (String hobby : hobbies){
-            $(byText(hobby)).click();
-        }
-
-        //Image
-        $("#uploadPicture").uploadFromClasspath("Mem.png");
-
-        //Address
-        $("#currentAddress").setValue(address);
-
-        //State
-        $("#state").scrollTo().click();
-        $(byText(state)).click();
-
-        //City
-        $("#city").click();
-        $(byText(city)).click();
-
-        //Submit
-        $("#submit").click();
-
+    void testFormWithPageObject() {
+        StudentRegistrationPageObject studentRegistration = new StudentRegistrationPageObject();
+        //Input
+        studentRegistration.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .enterSubjects(subjects)
+                .enterGender(gender)
+                .enterUserPhone(phone)
+                .enterBirthDate(day, month, year)
+                .enterSubjects(subjects)
+                .enterHobbys(hobbys)
+                .uploadPictureForm(photo)
+                .enterAddress(address)
+                .enterStateAndCity(state, city)
+                .pushSubmit();
 
         //Validate
-        $(".table-responsive").shouldHave(
-                text("Student Name " + firstName + " " + lastName),
-                text("Student Email " + email),
-                text("Gender " + gender.selectorValue),
-                text("Mobile " + phone),
-                text("Date of Birth " + day.substring(1) + " January," + year),
-                text("Subjects " + Array.get(subjects, 0) + ", " + Array.get(subjects, 1)),
-                text("Hobbies " + Array.get(hobbies, 0) + ", " + Array.get(hobbies, 1)),
-                text("Picture " + photo),
-                text("Address " + address),
-                text("State and City " + state + " " + city));
+        studentRegistration.validate(firstName)
+                .validate(lastName)
+                .validate(email)
+                .validate(phone)
+                .validate(year).validate(month).validate(day)
+                .validate(photo)
+                .validate(address)
+                .validate(state)
+                .validate(city);
+
+        for (String s : subjects) {
+            studentRegistration.validate(s);
+        }
+        for (String s : hobbys) {
+            studentRegistration.validate(s);
+        }
     }
 }
---
+++ src/test/java/pages/StudentRegistrationPageObject.java
@@ -0,0 +1,142 @@
+package pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+
+import java.lang.reflect.Array;
+
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class StudentRegistrationPageObject {
+    private String url = "/automation-practice-form";
+    private CalendarComponent calendarComponent = new CalendarComponent();
+    private SelenideElement
+            headerTitle = $(".main-header"),
+            fistNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            emailInput = $("#userEmail"),
+            userPhoneInput = $("#userNumber"),
+            subjectsInput = $("#subjectsInput"),
+            uploadPictureInput = $("#uploadPicture"),
+            currentAddressInput = $("#currentAddress"),
+            stateInput = $("#state"),
+            cityInput = $("#city"),
+            hobbySportsButton = $(byText("Sports")),
+            hobbyReadingButton = $(byText("Reading")),
+            hobbyMusicButton = $(byText("Music")),
+            genderOtherButton = $(byText("Other")),
+            genderMaleButton = $(byText("Male")),
+            genderFemaleButton = $(byText("Female")),
+            dateOfBirthButton = $("#dateOfBirthInput"),
+            submitButton = $("#submit"),
+            validateSelector = $(".table-responsive");
+
+
+    public StudentRegistrationPageObject openPage() {
+        open(url);
+        headerTitle.shouldHave(Condition.text("Practice form"));
+        return this;
+    }
+
+
+    public StudentRegistrationPageObject enterFirstName(String firstName) {
+        fistNameInput.setValue(firstName);
+        return this;
+    }
+
+    public StudentRegistrationPageObject enterLastName(String lastName) {
+        lastNameInput.setValue(lastName);
+        return this;
+    }
+
+    public StudentRegistrationPageObject enterEmail(String email) {
+        emailInput.setValue(email);
+        return this;
+    }
+
+    public StudentRegistrationPageObject enterGender(String gender) {
+        switch (gender) {
+            case ("Male"):
+                genderMaleButton.click();
+                break;
+            case ("Female"):
+                genderFemaleButton.click();
+                break;
+            case ("Other"):
+                genderOtherButton.click();
+                break;
+        }
+        return this;
+    }
+
+    public StudentRegistrationPageObject enterUserPhone(String userPhone) {
+        userPhoneInput.setValue(userPhone);
+        return this;
+    }
+
+    public StudentRegistrationPageObject enterBirthDate(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        calendarComponent.setDate(day, month, year);
+        return this;
+    }
+
+    public StudentRegistrationPageObject enterSubjects(String[] subjects) {
+        for (String subject : subjects) {
+            subjectsInput.setValue(subject.substring(0, 2)).click();
+            $(byText(subject)).click();
+        }
+        return this;
+    }
+
+    public StudentRegistrationPageObject enterHobbys(String[] hobbys) {
+        for (String hobby : hobbys) {
+            switch (hobby) {
+                case ("Sport"):
+                    hobbySportsButton.click();
+                    break;
+                case ("Reading"):
+                    hobbyReadingButton.click();
+                    break;
+                case ("Music"):
+                    hobbyMusicButton.click();
+                    break;
+            }
+        }
+        return this;
+    }
+
+    public StudentRegistrationPageObject uploadPictureForm(String fileName) {
+        uploadPictureInput.uploadFromClasspath(fileName);
+        return this;
+    }
+
+    public StudentRegistrationPageObject enterAddress(String address) {
+        currentAddressInput.setValue(address);
+        return this;
+    }
+
+    public StudentRegistrationPageObject enterStateAndCity(String state, String city) {
+        stateInput.scrollTo().click();
+        $(byText(state)).click();
+        cityInput.click();
+        $(byText(city)).click();
+        return this;
+    }
+
+    public StudentRegistrationPageObject pushSubmit() {
+        submitButton.click();
+        return this;
+    }
+
+    public StudentRegistrationPageObject validate(String input) {
+        validateSelector.shouldHave(Condition.text(input));
+        return this;
+    }
+
+
+}
+
