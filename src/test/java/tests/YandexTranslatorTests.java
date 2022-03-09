81675cc55ceafaa7c64df5e8f66652c2b456d58b
Alexkarav <djmyasoru@gmail.com>
2022-03-09 13:14:58 +0300

    Added 1 more parametrized test

--- src/test/java/tests/YandexTranslatorTests.java
+++ src/test/java/tests/YandexTranslatorTests.java
@@ -4,9 +4,14 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import pages.pageObjects.YandexTranslatorPageObject;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.browserSize;
@@ -21,12 +26,6 @@ public class YandexTranslatorTests {
         baseUrl = "https://translate.yandex.ru";
         browserSize = "1920x1280";
     }
-
-    @AfterAll
-    static void afterAll() {
-        closeWebDriver();
-    }
-
 
     @ParameterizedTest
     @CsvSource(value = {
@@ -58,7 +57,23 @@ public class YandexTranslatorTests {
             $(byId("urlInput-input")).setValue(site);
             $(".icn icn_arrow-short-forward").click();
         });
-
     }
 
+    static Stream<Arguments> mixedArgumentsTestDataProvider() {
+        return Stream.of(
+                Arguments.of("Русский", List.of("Английский", "Первый параметрезированный тест", "The first parameterized test")),
+                Arguments.of("Вьетнамский", List.of("Гаитянский", "Vinh quang đến Ukraine", "Tout bèl pouvwa a nan Ikrèn"))
+        );
+    }
+
+    @MethodSource(value = "mixedArgumentsTestDataProvider")
+    @ParameterizedTest(name = "Another method test")
+    void mixedArgumentsTest(String inputLang, List<String> secondArg) {
+        open(baseUrl);
+        YandexTranslatorPageObject yandexTranslator = new YandexTranslatorPageObject();
+        yandexTranslator.choiceInputLanguage(inputLang)
+                .choiceOutputLanguage(secondArg.get(0))
+                .inputText(secondArg.get(1))
+                .assertions(secondArg.get(2));
+    }
 }
