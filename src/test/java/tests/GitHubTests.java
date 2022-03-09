313966c4d83cd2fbd561b5f739c276c2e631bef0
Alexkarav <djmyasoru@gmail.com>
2022-03-09 15:04:36 +0300

    Add test with Allure Listener

--- src/test/java/tests/GitHubTests.java
+++ src/test/java/tests/GitHubTests.java
@@ -1,13 +1,21 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.codeborne.selenide.selector.ByText;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import pages.pageObjects.gitHub.GitHubSelenidePageObjects;
 import pages.pageObjects.gitHub.GitHubSelenideWikiPageObjects;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class GitHubTests {
 
@@ -27,5 +35,18 @@ public class GitHubTests {
         gitHubSelenideWikiPageObjects.checkText("SoftAssertions")
                 .clickSoftAssertions()
                 .checkJunit5Text("Using JUnit5 extend test class");
+    }
+
+    @Test
+    void IssueTestAllureListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://github.com");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(By.linkText("eroshenkoam/allure-example")).click();
+        $(By.partialLinkText("Issues")).click();
+        $(withText("#68")).should(Condition.exist);
     }
 }
